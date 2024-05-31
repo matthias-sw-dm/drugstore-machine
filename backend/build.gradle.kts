b1d6ab4aa69fbe92a0fdefbeb4d35ba35cc13ef6
Matthias Schultze-Westrum <matthias.schultze-westrum@dm.de>
2024-05-31 10:31:53 +0200

    add backend project

---
+++ backend/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.0"
+	id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "drugstore"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	implementation("org.flywaydb:flyway-core")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.hsqldb:hsqldb")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ backend/src/main/resources/db.migration/V1__Initial.sql
@@ -0,0 +1,7 @@
+CREATE TABLE products
+(
+    id  int PRIMARY KEY,
+    name    text,
+    product_group   text,
+    price   float
+);
